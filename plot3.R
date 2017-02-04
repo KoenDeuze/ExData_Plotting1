c8fe594483e4ac89db9c8316ae3d56dcd50de860
Koen Deuze <koendeuze@gmail.com>
2017-02-04 10:25:44 +0100

    version 20170204 1025

--- plot3.R
+++ plot3.R
@@ -23,11 +23,11 @@ energy_data$numMeter_3 <- as.numeric(as.character(energy_data$Sub_metering_3))
 energy_feb_2007 <- subset(energy_data, format.Date(NewDate, "%d") %in% c("01","02")  & format.Date(NewDate, "%m")=="02" & format.Date(NewDate, "%Y")=="2007")
 
 # create the chart
+png("plot3.png", width = 480, height = 480, type = "quartz")
 plot(energy_feb_2007$DateTime,energy_feb_2007$numMeter_1, type="n",xlab="", ylab = "Energy sub metering")
 lines(energy_feb_2007$DateTime,energy_feb_2007$numMeter_1, col="black")
 lines(energy_feb_2007$DateTime,energy_feb_2007$numMeter_2, col="red")
 lines(energy_feb_2007$DateTime,energy_feb_2007$numMeter_3, col="blue")
 legend("topright",lty=1 ,col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-dev.copy(png,file="plot2.png", width=480, height=480)
 dev.off()
