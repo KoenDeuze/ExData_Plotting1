649ff2c88bc2b842f2512eb32b9497af7aea4509
Koen Deuze <koendeuze@gmail.com>
2017-02-04 10:02:42 +0100

    version 20170204 1002

--- plot2.R
+++ plot2.R
@@ -4,8 +4,12 @@ download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
 energy_data <- read.csv2(unz(temp,"household_power_consumption.txt"),header = TRUE, sep = ";")
 unlink(temp)
 
+
 # convert the factor 'date' to a field of class Date
 energy_data$NewDate <- as.Date(energy_data$Date,format = "%d/%m/%Y")
+
+# create a combined date-time variable
+energy_data$DateTime <- as.POSIXct(paste(energy_data$Date, energy_data$Time), format="%d/%m/%Y %H:%M:%S") 
 
 # convert the factor 'GLobal Active Power' to a field of class numeric
 energy_data$newGAP <- as.numeric(as.character(energy_data$Global_active_power))
@@ -13,9 +17,9 @@ energy_data$newGAP <- as.numeric(as.character(energy_data$Global_active_power))
 # subset the dataset
 energy_feb_2007 <- subset(energy_data, format.Date(NewDate, "%d") %in% c("01","02")  & format.Date(NewDate, "%m")=="02" & format.Date(NewDate, "%Y")=="2007")
 
-# create the histogram
-hist(energy_feb_2007$newGAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", breaks = 25)
+# create the chart
+plot(energy_feb_2007$DateTime,energy_feb_2007$newGAP, type="n",xlab="", ylab = "Global Active Power (kilowatts)")
+lines(energy_feb_2007$DateTime,energy_feb_2007$newGAP)
 
-# save the histogram as PNG-file
-dev.copy(png,file="plot1.png", width=480, height=480)
+dev.copy(png,file="plot2.png", width=480, height=480)
 dev.off()
